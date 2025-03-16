fb82d6cbbcb9177b4cd78190fb6a7c962b514431
Paul <kimjs20020426@gmail.com>
2025-03-16 15:28:27 +0900

    update

--- 배열 두 배 만들기/sol.py
+++ 배열 두 배 만들기/sol.py
@@ -2,3 +2,9 @@ def solution(numbers):
     my_tuple = tuple(numbers)
     answer = 2 * my_tuple
     return answer
+
+    # 이 코드가 틀린 이유
+    파이썬에서 'tuple * 정수'는 tuple을 반복하는 연산이지 각 원소에 정수를 곱하는 연산이 아님.
+    ex. my_tuple = (1, 2, 3)
+        result = 2 * my_tuple
+        ==> 결과값은 (1, 2, 3, 1, 2, 3)
---
+++ 배열 두 배 만들기/sol1.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+
+    answer = []
+    my_list = list(numbers)
+    for i in my_list:
+        answer.append(i * 2)
+
+    return answer
